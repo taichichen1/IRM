fe1c14964b6bebce6c5286382c018b1bae829bd3
taichichen1 <103369792+taichichen1@users.noreply.github.com>
2022-05-08 10:49:45 -0400

    Update streamlitTest.py

--- streamlitTest.py
+++ streamlitTest.py
@@ -9,7 +9,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Uber pickups in NYC testing')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
